dd4bdc506d6b07d2978a04436594bc931d99939c
KluckZ <ckjr83@gmail.com>
2025-12-02 14:19:28 -0500

    fix: Acceder a atributos de Kerykeion correctamente como objetos

--- utils.py
+++ utils.py
@@ -11,22 +11,27 @@ def extract_planets(subject):
     planet_names = [
         'sun', 'moon', 'mercury', 'venus', 'mars',
         'jupiter', 'saturn', 'uranus', 'neptune', 'pluto',
-        'mean_node', 'true_node', 'chiron', 'pars_fortuna', 'asc', 'mc'
+        'mean_node', 'true_node', 'chiron', 'pars_fortuna'
     ]
 
+    # Planetas regulares
     for planet_name in planet_names:
         if hasattr(subject, planet_name):
             planet_obj = getattr(subject, planet_name)
-            # Verificar que planet_obj no sea None y sea un dict
-            if planet_obj and isinstance(planet_obj, dict):
-                planets.append({
-                    'name': planet_name,
-                    'sign': planet_obj.get('sign', ''),
-                    'position': planet_obj.get('position', 0.0),
-                    'abs_pos': planet_obj.get('abs_pos', 0.0),
-                    'house': planet_obj.get('house', ''),
-                    'retrograde': planet_obj.get('retrograde', False)
-                })
+            if planet_obj:
+                try:
+                    planets.append({
+                        'name': planet_name,
+                        'sign': getattr(planet_obj, 'sign', ''),
+                        'sign_num': getattr(planet_obj, 'sign_num', 0),
+                        'position': getattr(planet_obj, 'position', 0.0),
+                        'abs_pos': getattr(planet_obj, 'abs_pos', 0.0),
+                        'emoji': getattr(planet_obj, 'emoji', ''),
+                        'house': getattr(planet_obj, 'house', ''),
+                        'retrograde': getattr(planet_obj, 'retrograde', False)
+                    })
+                except Exception:
+                    pass
 
     return planets
 
@@ -35,18 +40,28 @@ def extract_houses(subject):
     """Extrae cúspides de casas del objeto AstrologicalSubject"""
     houses = []
 
-    for i in range(1, 13):
-        house_attr = f'first_house' if i == 1 else f'house{i}'
-        if hasattr(subject, house_attr):
-            house_obj = getattr(subject, house_attr)
-            # Verificar que house_obj no sea None y sea un dict
-            if house_obj and isinstance(house_obj, dict):
-                houses.append({
-                    'house_number': i,
-                    'sign': house_obj.get('sign', ''),
-                    'position': house_obj.get('position', 0.0),
-                    'abs_pos': house_obj.get('abs_pos', 0.0)
-                })
+    # Lista de casas (Kerykeion usa first_house para casa 1, second_house para casa 2, etc)
+    house_names = [
+        'first_house', 'second_house', 'third_house', 'fourth_house',
+        'fifth_house', 'sixth_house', 'seventh_house', 'eighth_house',
+        'ninth_house', 'tenth_house', 'eleventh_house', 'twelfth_house'
+    ]
+
+    for i, house_name in enumerate(house_names, 1):
+        if hasattr(subject, house_name):
+            house_obj = getattr(subject, house_name)
+            if house_obj:
+                try:
+                    houses.append({
+                        'house_number': i,
+                        'sign': getattr(house_obj, 'sign', ''),
+                        'sign_num': getattr(house_obj, 'sign_num', 0),
+                        'position': getattr(house_obj, 'position', 0.0),
+                        'abs_pos': getattr(house_obj, 'abs_pos', 0.0),
+                        'emoji': getattr(house_obj, 'emoji', '')
+                    })
+                except Exception:
+                    pass
 
     return houses
 
@@ -57,15 +72,22 @@ def extract_aspects(subject):
 
     if hasattr(subject, 'aspects_list') and subject.aspects_list:
         for aspect in subject.aspects_list:
-            # Verificar que aspect sea un dict
-            if aspect and isinstance(aspect, dict):
-                aspects.append({
-                    'planet1': aspect.get('p1_name', ''),
-                    'planet2': aspect.get('p2_name', ''),
-                    'aspect': aspect.get('aspect', ''),
-                    'orb': aspect.get('orb', 0.0),
-                    'diff': aspect.get('orbit', 0.0),
-                    'aid': aspect.get('aid', 0)
-                })
+            if aspect:
+                try:
+                    # Los aspectos son diccionarios en Kerykeion
+                    if isinstance(aspect, dict):
+                        aspects.append({
+                            'planet1': aspect.get('p1_name', ''),
+                            'planet2': aspect.get('p2_name', ''),
+                            'aspect': aspect.get('aspect', ''),
+                            'orbit': aspect.get('orbit', 0.0),
+                            'aspect_degrees': aspect.get('aspect_degrees', 0),
+                            'aid': aspect.get('aid', 0),
+                            'diff': aspect.get('orbit', 0.0),
+                            'p1_abs_pos': aspect.get('p1_abs_pos', 0.0),
+                            'p2_abs_pos': aspect.get('p2_abs_pos', 0.0)
+                        })
+                except Exception:
+                    pass
 
     return aspects
